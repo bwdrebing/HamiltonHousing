7ee2358eb79c23069667c9ca8756aea3e16a08c9
Claesen Wyckoff <cwyckoff@gemini.cs.hamilton.edu>
2016-09-29 16:48:17 -0400

    Testing models

--- staff/models.py
+++ staff/models.py
@@ -3,3 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(max_length = 100, primary_key = true)
+    totalRooms = models.PositiveSmallIntegerField()
+    totalFloors = models.PositiveSmallIntegerField()
+    totalSingles = models.PositiveSmallIntegerField()
+    totalDoubles = models.PositiveSmallIntegerField()
+    totalTriples = models.PositiveSmallIntegerField()
+    totalQuads = models.PositiveSmallIntegerField()
+    totalBeds = models.PositiveSmallIntegerField()
+    location = models.CharField(max_length = 100)
+    genderBlocked = models.BooleanField()
+    closed = models.BooleanField()
+    notes = models.TextField()
+    
+    def __str__(self):
+
+        return self.name
+    
