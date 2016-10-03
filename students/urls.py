00a4607c0d07ca9a08fe6583b986a00ff9d6ce76
Kelsey Babcock <kelseyebabcock@gmail.com>
2016-10-03 18:25:22 -0400

    Added Inner Zoom plug-in, side navigation and room tables to building page.

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^building/([a-zA-Z ]+)', views.building, name='building-view'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Building
+from .models import Room
 
 # Home page view
 def home(request):
-    return render(request, 'students/home.html', {})
+    buildings = list(Building.objects.all())
+    return render(request, 'students/home.html', {'buildings': buildings})
+
+def building(request, building_name):
+    bldg = Building.objects.get(name=building_name)
+    building_list = Building.objects.order_by('name')
+    #all the rooms that are in this building and that are available -- sort?
+    rooms = list(Room.objects.all().filter(building=bldg).exclude(available=False).order_by('number'))
+    return render(request, 
+                  'students/building.html', 
+                  {'current_building': bldg, 'building': bldg, 'rooms': rooms, 'buildings': building_list})
