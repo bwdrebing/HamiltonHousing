c8de4c76fc44ee78c9274782de40857d3ad1d2f2
Kelsey Babcock <kelseyebabcock@gmail.com>
2016-10-03 19:38:32 -0400

    Made floors dictionary that will populate dropdown with floor numbers in views.py

--- students/views.py
+++ students/views.py
@@ -11,7 +11,28 @@ def building(request, building_name):
     bldg = Building.objects.get(name=building_name)
     building_list = Building.objects.order_by('name')
     #all the rooms that are in this building and that are available -- sort?
-    rooms = list(Room.objects.all().filter(building=bldg).exclude(available=False).order_by('number'))
+    rooms = list(Room.objects.all()
+                             .filter(building=bldg)
+                             .exclude(available=False)
+                             .order_by('number'))
+    
+    # get floor numbers and create dictionary of rooms organized by floor
+    # ex. {0: [001, 002, 003], 1: [101, 102, 103]}
+    floors = {}
+    last_floor_seen = rooms[0].number[0] # last floor number seen
+    for room in rooms:
+        if room.number[0] != last_floor_seen:
+            last_floor_seen = room.number[0]
+            floors[room.number[0]] = [room]
+
+        else:
+            floors[room.number[0]].append(room) # floor already seen, append to list
+            
+            
     return render(request, 
                   'students/building.html', 
-                  {'current_building': bldg, 'building': bldg, 'rooms': rooms, 'buildings': building_list})
+                  {'current_building': bldg, 
+                   'building': bldg, 
+                   'allrooms': rooms, 
+                   'floors': floors,
+                   'buildings': building_list})
