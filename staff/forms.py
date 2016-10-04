e0949d8ba067ceeb9c4d35f37d144fc5397151e1
Ben <bwdrebing@gmail.com>
2016-10-04 17:25:58 -0400

    Added StudentInfoForm page :D

--- staff/forms.py
+++ staff/forms.py
@@ -10,9 +10,23 @@ class LotteryNumberForm(forms.ModelForm):
         fields = ('number',)
 
 class BuildingForm(forms.Form):
-    
+
+    #maybe just keep the id as first thing in tuple below
     name = forms.ChoiceField(
-                choices = [(o.id, str(o)) for o in list(Building.objects.all())])
-    lottery_Number = forms.IntegerField()
-    genderChoices = [("F","Female"),("M","Male")]
-    gender = forms.ChoiceField(choices=genderChoices)
+                choices = [(str(o), str(o)) for o in list(Building.objects.all())])
+    room_number = forms.IntegerField()
+
+
+class StudentInfoForm(forms.Form):
+
+    lottery_number = forms.IntegerField()
+
+    gender_choices = [('male', 'Male'), ('female', 'Female')]
+
+    gender = forms.ChoiceField(
+        choices = gender_choices
+    )
+
+    year_choices = [(num, num) for num in range(2014, 2019)]
+    
+    class_year = forms.ChoiceField(choices = year_choices)
--- staff/views.py
+++ staff/views.py
@@ -7,6 +7,7 @@ from .models import Building
 
 from .forms import LotteryNumberForm
 from .forms import BuildingForm
+from .forms import StudentInfoForm
 
 
 #Create your views here.
@@ -22,16 +23,25 @@ def lotteryNumberInput(request):
             {'LotteryNumber': number,'form' : form})
 
 def RoomSelect(request):
-    if request.method == "POST":
-        form = BuildingForm(request.POST)
-        if form.is_valid:
-           print("Building Chosen")
-           #Do some redirection here
     form = BuildingForm()
+    headerText = "Please enter student information to get started..."
+    if request.method == "POST":
+        responseForm = BuildingForm(request.POST)
+        if responseForm.is_valid():
+            #Would get building via its id here...
+            headerText = "Placing student in room " + \
+                         str(responseForm.cleaned_data['room_number']) +\
+                        " in building " + responseForm.cleaned_data['name'] + ":"
+
+            form = StudentInfoForm()
+
+
     buildings = list(Building.objects.all())
     number = list(LotteryNumber.objects.all())[-1]
     return render(request, 'staff/RoomSelect.html',
-            {'buildings' : buildings, 'LotteryNumber' : number, 'form' : form})
+            {'HeaderText' : headerText,
+             'LotteryNumber' : number,
+             'form' : form})
 
 
 def home(request):
