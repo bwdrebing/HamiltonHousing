97061aa1088c4e244431b4fa0d9a2494f3a53e14
Kelsey Babcock <kelseyebabcock@gmail.com>
2016-10-30 15:21:38 -0400

    Fixed the zoom issue but still not sure if it will work on mobile. Have yet to find a good plugin that works on mobile properly.

--- students/static/students/building/zoom.js
+++ students/static/students/building/zoom.js
@@ -1,4 +1,15 @@
+//initiate the zoom plugin and pass the id of the div containing gallery images
 $("#fullFloorPlan").elevateZoom({
-    zoomType: "inner",
-    cursor: "crosshair"
+    zoomType: 'inner',
+    cursor: 'crosshair',
+    gallery:'thumbnail-gallery',  
+    galleryActiveClass: 'active', 
+    imageCrossfade: true, 
+    loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'}); 
+
+//pass the images to Fancybox
+$("#fullFloorPlan").bind("click", function(e) {  
+  var ez =   $('#fullFloorPlan').data('elevateZoom');	
+	$.fancybox(ez.getGalleryList());
+  return false;
 });
