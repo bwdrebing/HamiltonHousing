90851d3630d2a7d9894bf39b0f17b389733bef8a
Kelsey Babcock <kelseyebabcock@gmail.com>
2016-11-02 01:35:05 -0400

    when screen is small enough that floor plans are at bottom of screen, a button appears at the top with a link to an anchor at the floor plan section

--- staff/urls.py
+++ staff/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # lottery number form
     url(r'^lotterynumber$', views.lotteryNumberInput, name='loto-num'),
+    
+    # select forms
     url(r'select$', views.select, name='select'),
     url(r'select/suite$', views.suiteSelect, name='suite-select'),
     url(r'select/suite/studentinfo$', views.suiteStudentInfo, name='suite-select-student-info'),
@@ -10,14 +13,20 @@ urlpatterns = [
     url(r'^select/room$', views.RoomSelect, name='room-select'),
     url(r'^select/room/studentinfo$', views.StudentInfo, name='room-select-student-info'),
     url(r'^select/room/confirmselection$', views.ConfirmSelection, name='room-select-confirm'),
-    url(r'^home$', views.home, name='home'),
+    
+    # edit forms
     url(r'^edit/transaction$', views.ReviewRoom, name='review-room'),
     url(r'^edit/transaction/studentinfo$', views.ReviewStudentInfo, name='review-room-student-info'),
     url(r'^edit/transaction/confirm$', views.ConfirmSelection, name='review-room-confirm'),
     url(r'^edit$', views.edit, name='edit'),
     url(r'^edit/building$', views.editBuilding, name = 'edit-building'),
     url(r'^edit/room$', views.editRoom, name = 'edit-room'),
+    
+    # login/out
     url(r'^login', views.userLogin, name='login'),
     url(r'^logout$', views.userLogout, name='logout'),
+    
+    # home
+    url(r'^home$', views.home, name='home'),
     url(r'^$', views.home, name='home'),
 ]
