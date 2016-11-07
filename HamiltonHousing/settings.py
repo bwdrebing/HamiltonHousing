20f183cc365b498c07d92be070ab934f0442be15
kelsey <kelseyebabcock@gmail.com>
2016-11-07 15:24:17 -0500

    Add WSGI App

--- HamiltonHousing/settings.py
+++ HamiltonHousing/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '9g@=9w)9d)^h52l_2i26rdct#(nuf_tc8$2@txzh4dx2l8_gzd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['150.209.91.51']
 
 
 # Application definition
