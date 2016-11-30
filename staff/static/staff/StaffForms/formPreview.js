2a951bbc4538db10ef83cb723ff977bcb600544f
bwdrebing <bwdrebing@gmail.com>
2016-11-30 03:56:17 -0500

    Update form confirmation code

--- staff/static/staff/StaffForms/formPreview.js
+++ staff/static/staff/StaffForms/formPreview.js
@@ -15,6 +15,39 @@ studentInfoConfirmText.js. Remember to include this textFunction in the template
     return text;
  };
 */
+function activateTab(tab){
+    $('.nav-pills a[href="#' + tab + '"]').tab('show');
+};
+
+
+var locateInvalidInputs = function() {
+  var formGroups = $('.form-group');
+  var alertText = "<ul>";
+  formGroups.each(function(index){
+
+    //Get any inputs within this form group
+    var input = $("select, input", this);
+    if(input.size() > 0){
+      if(input.prop('required') && !(input[0].validity.valid)){
+        console.log(index);
+        var parentTab = $(this).parents(".tab-pane");
+        console.log(parentTab.attr('id'));
+        activateTab(parentTab.attr('id'));
+
+        alertText += "<li><strong>"
+                      + $('#room-nav .active a').html() + " "
+                      + $("label", this).html() +
+                      "</strong></li>";
+      }
+    }
+  });
+  alertText += "</ul>";
+  $.alert({
+    title : " You forgot to fill out:",
+    content : alertText,
+  });
+};
+
 
 $('.preview').on('click', function(event) {
     
