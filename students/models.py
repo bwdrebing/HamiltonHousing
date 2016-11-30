8b8746e835e9358e3d4186c8ab7a2f9215826cfc
bwdrebing <bwdrebing@gmail.com>
2016-11-30 04:28:33 -0500

    Update models.py

--- students/models.py
+++ students/models.py
@@ -27,14 +27,24 @@ class StudentPageContent(models.Model):
         help_text="This is the name of the current lottery; it will display on the home page"
     )
     
-    lottery_text = models.TextField(
-        default='',
-        help_text="This is a description of the logistics of the this lottery"
-    )
-    
     contact = models.TextField(
         default='',
+        verbose_name="Contact Information",
         help_text="This is a block of text explaining who to contact with any immediate questions."
+    )
+    
+    contact_email = models.CharField(
+        max_length=100,
+        default='', 
+        blank=True,
+        help_text="This is the e-mail of the main person to contact with questions."
+    )
+    
+    navbar_announcement = models.TextField(
+        max_length=200,
+        default='',
+        blank=True,
+        help_text="This announcement will be displayed in a thin bar below the header navigation bar"
     )
     
     active = models.BooleanField(default=True)
